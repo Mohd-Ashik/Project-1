0d86368b15b99132d8d8c7e5d1f84779d56e561e
Mohd-Ashik <mdashik34@gmail.com>
2023-02-14 23:30:36 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-
+// mobile menubar open 
 function openMenu(){
     document.getElementById("navbar").style.left = "0px";
 }
-
-
+// mobile menubar close 
 function closeMenu(){
     document.getElementById("navbar").style.left = "-280px";
 }
+
+// sticky navbar 
+window.addEventListener('scroll',function(){
+    let navbar=document.getElementById("navbar");
+    if(window.pageYOffset >=112.9){
+        navbar.classList.add("sticky")
+    }else{
+        navbar.classList.remove("sticky");
+    }
+});
